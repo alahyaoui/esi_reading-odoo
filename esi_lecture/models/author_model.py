6d922899218f5d6f3a264f8c2827c3b8d965442b
ayoub lahyaoui <ayoub.lahyaoui@hotmail.com>
2021-12-03 20:41:51 +0100

    Fix Graph View.

--- esi_lecture/models/author_model.py
+++ esi_lecture/models/author_model.py
@@ -1,7 +1,15 @@
-from odoo import models, fields
+from odoo import fields, models, api, exceptions
 
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     is_author = fields.Boolean(string='Is Author?', default=False)
+
+    books_ids = fields.Many2many('reading.book', string="Books")
+    books_count = fields.Integer(string="Books count", compute='_get_books_count', store=True)
+
+    @api.depends('books_count')
+    def _get_books_count(self):
+        for r in self:
+            r.books_count = len(r.books_ids)
