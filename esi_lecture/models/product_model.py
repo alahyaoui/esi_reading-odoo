c2df8eeef53c779eb58a4fca0e4e57299ecbe13b
Aharag Mariane <54010@etu.he2b.be>
2021-12-13 15:22:05 +0100

    change string

--- esi_lecture/models/product_model.py
+++ esi_lecture/models/product_model.py
@@ -4,4 +4,4 @@ from odoo import fields, models, api, exceptions
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    product_books = fields.One2many('reading.book', string="Livres associées")
+    product_books = fields.One2many('reading.book', string="Livre(s)")
