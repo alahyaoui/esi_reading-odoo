6a7f7f564c38542eba7d82c42dc0ed87923ec72f
ayoub lahyaoui <ayoub.lahyaoui@hotmail.com>
2022-01-25 11:04:45 +0100

    g54895 : Les vues

--- esi-lecture-rent/__manifest__.py
+++ esi-lecture-rent/__manifest__.py
@@ -24,9 +24,11 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/member.xml',
+        'views/rent.xml',
+        'views/book.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
