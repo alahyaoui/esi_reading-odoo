f27dd7f2c0e1da93e3f378e0b8d9d3b5bdc55b7c
ayoub lahyaoui <ayoub.lahyaoui@hotmail.com>
2022-01-25 11:41:55 +0100

    Règle métiers

--- esi-lecture-rent/models/rent_model.py
+++ esi-lecture-rent/models/rent_model.py
@@ -16,6 +16,12 @@ class Rent(models.Model):
     book_id = fields.Many2one('reading.book', string='Livre prêté', required=True)
     member_id = fields.Many2one('res.partner', string='Membre', required=True)
 
+    def set_lost(self):
+        self.state = 'lost'
+
+    def set_returned(self):
+        self.state = 'rented'
+
     def write(self, values):
         if values['state'] == 'rented' and self.state != 'returned':
             raise exceptions.ValidationError('Le livre doit etre retourné avant de pouvoir etre de nouveau emprunté.')
